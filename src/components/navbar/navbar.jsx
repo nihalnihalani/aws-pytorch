b3237106d9deb1e2a50dd6f223c2289d3f311460
Akash Nemade <37910311+sskkyy962@users.noreply.github.com>
2020-08-22 14:01:17 -0700

    navbar is responsive

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,34 +5,36 @@ import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
 	appbar: {
-		backgroundColor: 'purple',
+		backgroundColor:'purple',
 		flexGrow: 1
 	},
 	title: {
 		flexGrow: 1
 	},
 	button:{
-		padding:1
+		padding:6,
+		color:'inherit',
+		variant:'contained'
 	}
 }));
 
 export default function Navbar() {
 	const akash = useStyles();
 	return (
-		<div className={akash.appbar}>
-			<AppBar position="static">
+		<div >
+			<AppBar className={akash.appbar} position="static">
 				<Toolbar>
-					<Typography className={akash.title} variant="h6">
+					<Typography className={akash.title} variant="h4">
 						Ai-Radiology
 					</Typography>
 
-					<Button variant='contained' component={Link} to="/">
+					<Button className={akash.button}  component={Link} to="/">
 						Home
 					</Button>
-					<Button variant='contained' component={Link} to="/about">
+					<Button className={akash.button}  component={Link} to="/about">
 						About
 					</Button>
-					<Button variant='contained' component={Link} to="/team">
+					<Button className={akash.button}  component={Link} to="/team">
 						Team
 					</Button>
 				</Toolbar>
