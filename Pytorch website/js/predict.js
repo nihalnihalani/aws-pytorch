9cced017600ba02bae308a01b0e54583d05f6db3
NIHAL NIHALANI <nihal.nihalani@gmail.com>
2020-08-31 08:26:53 -0700

    aws addition

--- Pytorch website/js/predict.js
+++ Pytorch website/js/predict.js
@@ -1,48 +1,58 @@
 $("#image-selector").change(function () {
-	let reader = new FileReader();
+    
+    console.log( "Selecting Image...");
+    let reader = new FileReader();
 	reader.onload = function () {
 		let dataURL = reader.result;
 		$("#selected-image").attr("src", dataURL);
-		$("#prediction-list").empty();
+ 		$("#prediction-list").empty();
 	}
 	
 	let file = $("#image-selector").prop('files')[0];
 	reader.readAsDataURL(file);
 });
 
-let model;
-$( document ).ready(async function () {
-	$('.progress-bar').show();
-    console.log( "Loading model..." );
-    model = await tf.loadGraphModel('model/model.json');
-    console.log( "Model loaded." );
-	$('.progress-bar').hide();
-});
 
 $("#predict-button").click(async function () {
+//  
+    
+    AWS.config.region = 'us-east-1'; // Region
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: 'us-east-1:557905a9-218b-4e68-95cf-e8141aca5ebb',
+    });
+    
+    const buffer = require('buffer');
 	let image = $('#selected-image').get(0);
+    
+    console.log(image);
 	
+
 	// Pre-process the image
 	console.log( "Loading image..." );
-	let tensor = tf.browser.fromPixels(image, 3)
-		.resizeNearestNeighbor([224, 224]) // change the image size
-		.expandDims()
-		.toFloat()
-		.reverse(-1); // RGB -> BGR
-	let predictions = await model.predict(tensor).data();
-	console.log(predictions);
-	let top5 = Array.from(predictions)
-		.map(function (p, i) { // this is Array.map
-			return {
-				probability: p,
-				className: TARGET_CLASSES[i] // we are selecting the value from the obj
-			};
-		}).sort(function (a, b) {
-			return b.probability - a.probability;
-		}).slice(0, 2);
+	let tensor = tf.browser.fromPixels(image, 1)
+		.resizeNearestNeighbor([256, 256]) // change the image size
 
-	$("#prediction-list").empty();
-	top5.forEach(function (p) {
-		$("#prediction-list").append(`<li>${p.className}: ${p.probability.toFixed(6)}</li>`);
-		});
+        
+                    
+    var sagemakerruntime = new AWS.SageMakerRuntime({region:'us-east-2',});
+    
+    var params = {
+        Body: image['src'].split(",")[1], /* Strings will be Base-64 encoded on your behalf */ /* required */
+        EndpointName: 'pytorch-inference-2020-08-25-19-48-21-336', /* required */
+        Accept: 'application/json',
+        ContentType: 'application/x-image'
+
+    };
+    sagemakerruntime.invokeEndpoint(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);
+                                    
+            $("#prediction-list").append(new TextDecoder("utf-8").decode(data['Body']));
+            // !! This string is the prediction !!
+            console.log($("#prediction-list"));
+    });
+    
+        
 });
+
+
